7482ca16dadecccf9ce95bce6c6dc9c48791c790
PC-AMPLI144 <teynard@amplitude-studios.com>
2021-12-07 18:01:59 +0100

    Day 3

---
+++ day3/src/main.rs
@@ -0,0 +1,89 @@
+fn main() {
+    let input: Vec<&str> = include_str!("../input.txt").lines().collect();
+    let mut output = (0, 0);
+
+    // Gamma & Epsilon rates.
+    let mut zero_counts = [0; 12];
+    let mut one_counts = [0; 12];
+    let mut char_index;
+    for line in &input {
+        char_index = 0;
+        for char in line.chars() {
+            if char == '0' {
+                zero_counts[char_index] += 1;
+            } else {
+                one_counts[char_index] += 1;
+            }
+
+            char_index += 1;
+        }
+    }
+
+    let mut gamma_bits = String::new();
+    let mut epsilon_bits = String::new();
+    for i in 0..zero_counts.len() {
+        if one_counts[i] > zero_counts[i] {
+            gamma_bits.push('1');
+            epsilon_bits.push('0');
+        } else {
+            epsilon_bits.push('1');
+            gamma_bits.push('0');
+        }
+    }
+
+    let gamma_rate = isize::from_str_radix(gamma_bits.as_str(), 2).unwrap();
+    let epsilon_rate = isize::from_str_radix(epsilon_bits.as_str(), 2).unwrap();
+    output.0 = gamma_rate * epsilon_rate;
+
+    // Oxygen Generator rating.
+    char_index = 0;
+    let mut candidates = input.to_vec();
+    while candidates.len() > 1 {
+        let half_count = candidates.len() / 2;
+        let wanted = if candidates
+            .iter()
+            .filter(|x| x.chars().nth(char_index).unwrap() == '1')
+            .count()
+            >= half_count
+        {
+            '1'
+        } else {
+            '0'
+        };
+
+        candidates.retain(|&x| x.chars().nth(char_index).unwrap() == wanted);
+
+        println!("Candidate Count {}", candidates.len());
+        char_index += 1;
+    }
+
+    let oxygen_rating = isize::from_str_radix(candidates.last().unwrap(), 2).unwrap();
+
+    // CO2 Scrubber rating
+    char_index = 0;
+    candidates = input.to_vec();
+    while candidates.len() > 1 {
+        let half_count = candidates.len() / 2;
+        let wanted = if candidates
+            .iter()
+            .filter(|x| x.chars().nth(char_index).unwrap() == '1')
+            .count()
+            < half_count
+        {
+            '1'
+        } else {
+            '0'
+        };
+
+        candidates.retain(|&x| x.chars().nth(char_index).unwrap() == wanted);
+
+        println!("Candidate Count {}", candidates.len());
+        char_index += 1;
+    }
+
+    let co2_rating = isize::from_str_radix(candidates.last().unwrap(), 2).unwrap();
+    output.1 = oxygen_rating * co2_rating;
+
+    println!("Part 1 = {}", output.0);
+    println!("Part 2 = {}", output.1);
+}
